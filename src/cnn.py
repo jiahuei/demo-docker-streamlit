b100458b318804e0aae535a74808fc97630d3046
Tan Jia Huei <tanjiahuei@gmail.com>
2021-09-19 17:58:59 +0800

    + cnn example, `utils.py`

---
+++ src/cnn.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import torch
+from torchvision import models, transforms as T
+from PIL import Image
+from utils import imagenet_idx_to_label
+
+model = models.mobilenet_v3_small(pretrained=True)
+model.eval()
+
+transforms = T.Compose(
+    [
+        T.ToTensor(),
+        T.Resize(256),
+        T.CenterCrop(224),
+        T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
+
+
+# Input image
+fpath = "resources/n01819313_sulphur-crested_cockatoo.jpg"
+img = Image.open(fpath)
+
+# Run the net
+img = transforms(img)
+logits = model(img.unsqueeze(0))
+cls_id = torch.argmax(logits, -1)
+label = imagenet_idx_to_label(cls_id)
+assert label == "sulphur-crested cockatoo"
+
+print(f"{logits.shape = }")
+print(f"Predicted label: {label}")
---
+++ src/utils.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import json
+import requests
+from functools import lru_cache
+from PIL import Image
+from io import BytesIO
+
+
+@lru_cache(1)
+def load_imagenet_labels_1k():
+    with open("resources/imagenet_labels_1k.json", "r") as f:
+        return json.load(f)
+
+
+def imagenet_idx_to_label(idx):
+    return load_imagenet_labels_1k()[idx]
+
+
+def load_pil_image_from_url(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    return img
